5ef468a4354b6ffaf5efa22b5892ac0aab679687
chakri68 <chakridevireddy69@gmail.com>
2023-10-04 16:37:50 +0530

    Added starfield with outlet to routes

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,75 +4,70 @@ import {
   DoubleRightOutlined,
   PicCenterOutlined,
 } from "@ant-design/icons";
-import { Button, Tooltip, Space } from "antd";
+import { Button, Space } from "antd";
 import { Row } from "antd";
 import { useNavigate } from "react-router-dom";
-import Starfield from "./Starfield"
-import '../CSS/Home.css'
+import "../CSS/Home.css";
 
-const Home = (props) => {
+const Home = () => {
   const navigate = useNavigate();
 
-
   return (
-    <>
-      <Starfield />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          marginTop: "10%",
-        }}
-      >
-        <Row>
-          <Button
-            span={12}
-            type="primary"
-            shape="circle"
-            style={{ margin: 20 }}
-            onClick={() => {
-              navigate("/Signin");
-            }}
-            icon={<DoubleRightOutlined />}
-          />
-          <Button
-            span={12}
-            type="primary"
-            shape="circle"
-            style={{ margin: 20 }}
-            onClick={() => {
-              navigate("/Signup");
-            }}
-            icon={<DoubleLeftOutlined />}
-          />
-        </Row>
-        <Row>
-          <span span={12} style={{ margin: 20 }} className="whitetxt">
-            Login
-          </span>
-          <span span={12} style={{ margin: 20 }} className="whitetxt">
-            Register
-          </span>
-        </Row>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "10%",
+      }}
+    >
+      <Row>
+        <Button
+          span={12}
+          type="primary"
+          shape="circle"
+          style={{ margin: 20 }}
+          onClick={() => {
+            navigate("/Signin");
+          }}
+          icon={<DoubleRightOutlined />}
+        />
+        <Button
+          span={12}
+          type="primary"
+          shape="circle"
+          style={{ margin: 20 }}
+          onClick={() => {
+            navigate("/Signup");
+          }}
+          icon={<DoubleLeftOutlined />}
+        />
+      </Row>
+      <Row>
+        <span span={12} style={{ margin: 20 }} className="whitetxt">
+          Login
+        </span>
+        <span span={12} style={{ margin: 20 }} className="whitetxt">
+          Register
+        </span>
+      </Row>
 
-        <Space direction="vertical">
-          <Space wrap>
-            <Button
-              type="primary"
-              icon={<PicCenterOutlined />}
-              size="medium"
-              onClick={() => {
-                navigate("/Main");
-              }}
-            >
-              Enter Without Login
-            </Button>
-          </Space>
+      <Space direction="vertical">
+        <Space wrap>
+          <Button
+            type="primary"
+            icon={<PicCenterOutlined />}
+            size="medium"
+            onClick={() => {
+              navigate("/Main");
+            }}
+          >
+            Enter Without Login
+          </Button>
         </Space>
-      </div>
-    </>
+      </Space>
+    </div>
   );
 };
 
--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { useState } from "react";
 import { Button, Checkbox, Form, Input } from "antd";
-import { LockTwoTone , MailTwoTone } from "@ant-design/icons";
+import { LockTwoTone, MailTwoTone } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
-import Starfield from "./Starfield";
-import '../CSS/Signin.css'
+import "../CSS/Signin.css";
 
 // require("dotenv").config();
 // const LOGIN = process.env.LOGIN;
@@ -18,7 +17,7 @@ const Signin = (props) => {
   const navigate = useNavigate();
 
   const [credentials, setcredentials] = useState({ email: "", password: "" });
-  const [loading,isloading] = useState(0);
+  const [loading, isloading] = useState(0);
 
   const handlesubmit = async (e) => {
     isloading(true);
@@ -36,7 +35,6 @@ const Signin = (props) => {
         }),
       });
 
-
       // Parse the response body as JSON
       const data = await response.json();
 
@@ -44,15 +42,13 @@ const Signin = (props) => {
       console.log("Response data:", data);
       if (data.success) {
         localStorage.setItem("token", data.jwtToken);
-        navigate("/Main");
         props.showAlert("Successful Login", "success");
-
+        navigate("/Main");
       } else {
         props.showAlert("Invalid credentials", "error");
       }
     } catch (error) {
       console.log("Error:", error);
-      
     }
 
     isloading(false);
@@ -64,95 +60,89 @@ const Signin = (props) => {
   // }
 
   return (
-    <>
-      <Starfield />
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh",
-        }}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
       >
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
+        <h1
+          className="whitetxt specialtxt"
+          style={{ display: "flex", justifyContent: "center" }}
         >
-          <h1
-            className="whitetxt specialtxt"
-            style={{ display: "flex", justifyContent: "center" }}
-          >
-            {" "}
-            Login{" "}
-          </h1>
-          <Form.Item
-            name="email"
-            value={credentials.email}
+          {" "}
+          Login{" "}
+        </h1>
+        <Form.Item
+          name="email"
+          value={credentials.email}
+          onChange={(e) => {
+            setcredentials({ ...credentials, ["email"]: e.target.value });
+            console.log(credentials);
+          }}
+          rules={[{ required: true, message: "Please input your Email!" }]}
+        >
+          <Input
+            prefix={<MailTwoTone className="site-form-item-icon" />}
+            placeholder=" Email"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input
+            prefix={<LockTwoTone className="site-form-item-icon" />}
+            type="password"
+            placeholder=" Password"
+            value={credentials.password}
             onChange={(e) => {
-              setcredentials({ ...credentials, ["email"]: e.target.value });
+              setcredentials({
+                ...credentials,
+                ["password"]: e.target.value,
+              });
               console.log(credentials);
             }}
-            rules={[{ required: true, message: "Please input your Email!" }]}
-          >
-            <Input
-              prefix={<MailTwoTone className="site-form-item-icon" />}
-              placeholder=" Email"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[{ required: true, message: "Please input your Password!" }]}
-          >
-            <Input
-              prefix={<LockTwoTone className="site-form-item-icon" />}
-              type="password"
-              placeholder=" Password"
-              value={credentials.password}
-              onChange={(e) => {
-                setcredentials({
-                  ...credentials,
-                  ["password"]: e.target.value,
-                });
-                console.log(credentials);
-              }}
-            />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox className="whitetxt">Remember me</Checkbox>
-            </Form.Item>
-
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox className="whitetxt">Remember me</Checkbox>
           </Form.Item>
 
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-              onClick={handlesubmit}
-              style={{ marginRight: 10 }}
-              loading={loading}
-            >
-              Log in
-            </Button>
-            <span className="whitetxt specialtxt"> Or </span>
-            <a
-              href=""
-              onClick={() => {
-                navigate("/Signup");
-              }}
-            >
-              register now!
-            </a>
-          </Form.Item>
-        </Form>
-      </div>
-    </>
+          <a className="login-form-forgot">Forgot password</a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            onClick={handlesubmit}
+            style={{ marginRight: 10 }}
+            loading={loading}
+          >
+            Log in
+          </Button>
+          <span className="whitetxt specialtxt"> Or </span>
+          <a
+            onClick={() => {
+              navigate("/Signup");
+            }}
+          >
+            register now!
+          </a>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 export default Signin;
