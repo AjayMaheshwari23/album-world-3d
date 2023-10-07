9396652e5199e66cee55a73fc3521ca02a400e60
Ajay <lci2021023@iiitl.ac.in>
2023-10-07 15:35:10 +0530

    Minor Fixes

--- frontend/src/components/molecules/Gallerysvg.js
+++ frontend/src/components/molecules/Gallerysvg.js
@@ -4,11 +4,11 @@ const Gallerysvg = () => {
   return (
     <div>
       <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g
           id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></g>
         <g id="SVGRepo_iconCarrier">
           {" "}
